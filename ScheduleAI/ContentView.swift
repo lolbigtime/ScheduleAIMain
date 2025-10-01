1533e027871a4ae6a5bcd99f41f0dc637172b6f1
lolbigtime <pooplolbigtimetroll@gmail.com>
2025-10-01 00:21:38 -0500

    Initial Commit

---
+++ ScheduleAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScheduleAI
+//
+//  Created by Tai Wong on 10/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
